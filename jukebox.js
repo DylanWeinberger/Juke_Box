a31f7fcb6e6d4158aab1f991511dd88da769c8e2
Kyle N. Smith <smith.kylen@gmail.com>
2016-01-22 11:11:00 -0500

    updated JS

--- jukebox.js
+++ jukebox.js
@@ -1,130 +1,234 @@
+$(document).ready(function(){
+	function Jukebox () {
+		this.current_song = 0
+		this.all_tracks = ["1.mp3","2.mp3"]
+		
+		this.playlist = [];
+		this.new_track = function () {
+
+		// 	var x = document.getElementById("audiofile").value;
+		// var audio = new Audio(x);
+		
+			$("#name").html("<audio><source src= " + this.all_tracks[this.current_song] + " type='audio/mpeg'></audio>")
+	
+		}
+		this.play_current_song = function () {
+			// console.log("play")
+			$('audio')[0].play()
+		};
+		this.pause_current_song = function () {
+			// console.log("pause")
+			$('audio')[0].pause()
+		}
+		this.stop_current_song = function () {
+			// console.log("restart")
+			$('audio')[0].currentTime = 0;
+		}
+		
+	}
+
+	 
+
+	song = new Jukebox
+	song.new_track()
+
+	document.getElementById("load").onclick = function load_song(){
+		var x = document.getElementById("audiofile").value;
+		song.current_song = x
+		song.new_track()
+		console.log(song.current_song)
+	}
+
+	
+	document.getElementById("play").onclick=function () {
+		// console.log("click play")
+		song.play_current_song()
+	}
+
+	document.getElementById("pause").onclick=function () {
+		// console.log("click pause")
+		song.pause_current_song()
+	}
+
+	document.getElementById("stop").onclick=function () {
+		
+		song.stop_current_song()
+		song.pause_current_song()
+	}
+
+	document.getElementById("next_song").onclick=function () {
+		// console.log("click next")
+		
+		if (song.current_song == song.all_tracks.length){
+			song.current_song = 0
+		}
+		else (){ song.current_song += 1
+		}
+		song.new_track()
+	}
+
+	$('#random_button').click(function () {
+		// console.log("click next")
+		song.current_song = Math.floor(Math.random()*song.all_tracks.length);
+		song.new_track()
+	})
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+// OLD
+
+
+
 // $(document).ready(function(){
-// 	// $(".jukebox_div").hide();
 
-// 	function play_audio(task) {
-// 		if(task == 'play') {
-// 			$(".jukebox").trigger('play');
-// 		}
-// 		if(task == 'stop') {
-// 			$(".jukebox").trigger('pause');
-// 			$(".jukebox").prop("current_time", 0);
-// 		}
+// Current track is first place in array of all tracks
+// this.alltracks is hardcoded array
+// this.playcurrent track will play current track
+
+// this.pause will pause current track
+// this.stop
+
+// function Jukebox(song) {
+// 	this.song = 0
+// 	this.songlist = ["1.mp3", "2.mp3"]
+
+
+
+// 	this.play = function(){
+
 // 	}
-// });
 
- // var currentFile = "";
- //        function playAudio() {
- //            // Check for audio element support.
- //            if (window.HTMLAudioElement) {
- //                try {
- //                    var oAudio = document.getElementById('myaudio');
- //                    var btn = document.getElementById('play'); 
- //                    var audioURL = document.getElementById('audiofile'); 
+// 	this.add_song = function(song)
 
- //                    //Skip loading if current file hasn't changed.
- //                    if (audioURL.value !== currentFile) {
- //                        oAudio.src = audioURL.value;
- //                        currentFile = audioURL.value;                       
- //                    }
-
- //                    // Tests the paused attribute and set state. 
- //                    if (oAudio.paused) {
- //                        oAudio.play();
- //                        btn.textContent = "Pause";
- //                    }
- //                    else {
- //                        oAudio.pause();
- //                        btn.textContent = "Play";
- //                    }
- //                }
- //                catch (e) {
- //                    // Fail silently but show in F12 developer tools console
- //                     if(window.console && console.error("Error:" + e));
- //                }
- //            }
- //        }
-
-
-function Jukebox(songs) {
-
- 	var x = document.getElementById("audiofile").value;
-	var audio = new Audio(x);
-	this.songs = [];	
-	document.getElementById("play").onclick = function play(){
-		console.log(audio);
-		audio.play();
-		console.log("test");
-	}
-
-	document.getElementById("stop").onclick= function stop(){
-
-
-		console.log(audio);
-		audio.pause();
-		audio.currentTime = 0;
-		console.log("test");
-	}
-
-	document.getElementById("pause").onclick= function pause(){
-
-
-		console.log(audio);
-		audio.pause();
-		console.log("test");
-	}
-
-
-	document.getElementById("add_song").onclick= function add_song(){
-
- 	var x = document.getElementById("audiofile").value;
-	var audio = new Audio(x);
-	this.songs.push(songs);
-
-	}
-
-	
-	// this.add_song = function(x){
-	// this.songs.push(x);
-	// }
-}
-
-// function load(){
-	
-// 	var audioURL = document.getElementById('audiofile');
-
-// };
-
-// function Song(file) {
-// 	this.file = getElementById('audioFile').value;
 // }
 
-// $(".addsong_button").click(function Song(){
 
-// function load(){
+
+// function Song() {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// // var x = document.getElementById("myAudio").ended;
+
+
+// function Jukebox(songs) {
+// 	this.songs = songs;
+// 	var x = document.getElementById("audiofile").value;
+// 	var audio = new Audio(x);
+	
+// 	songs.push(audio);
+
+// 	console.log(songs)
+
+// document.getElementById("add_song").onclick = function create_song(){
 
 // var x = document.getElementById("audiofile").value;
-// var audio = new Audio(x);
-// // audio.play();
-
-// console.log("tester");
-//  // onclick pause do x
-// document.getElementById("pause").onclick = function(){
-		
-// 		console.log(audio);
-// 		audio.pause();
-// 		console.log("test");
-// 	};
-
-// document.getElementById("play").onclick = function(){
-		
-// 		console.log(audio);
-// 		audio.play();
-// 		console.log("test");
-// 	};
+// 	var audio = new Audio(x);
+// 	var new_song = audio.create
 	
+// 	songs.push(new_song);
 
-// };
+// }
+// 	// if audio.ended = true
+
+// var f = new Jukebox(["hello.mp3"])
+// // Captures song times.
+// var length = formatSecondsAsTime(audio.duration);
+// document.getElementById("length").innerHTML = length;
+
+// var time = formatSecondsAsTime(audio.currentTime);
+// document.getElementById("time").innerHTML = time;
 
 
 
 
+// document.getElementById("play").onclick = function play(){
+// 	console.log(audio);
+// 	// audio.play();
+// 	songs[0].play();
+// 	console.log("test");
+// 	console.log(songs);
+
+
+
+// }
+
+// document.getElementById("stop").onclick= function stop(){
+
+
+// 	console.log(audio);
+// 	audio.pause();
+// 	audio.currentTime = 0;
+// 	console.log("test");
+// }
+
+// document.getElementById("pause").onclick= function pause(){
+
+
+// 	console.log(audio);
+// 	audio.pause();
+// 	console.log("test");
+// }
+
+
+// document.getElementById("add_song").onclick= function add_song(){
+
+// 	var x = document.getElementById("audiofile").value;
+// 	var audio = new Audio(x);
+// 	this.songs.push(songs);
+
+// }
+
+
+// 	// this.add_song = function(x){
+// 	// this.songs.push(x);
+// 	// }
+// }
+
+
+
+
+
+
+
+// // Will render times in hh:mm:sec format
+
+//  function formatSecondsAsTime(secs, format) {
+//   var hr  = Math.floor(secs / 3600);
+//   var min = Math.floor((secs - (hr * 3600))/60);
+//   var sec = Math.floor(secs - (hr * 3600) -  (min * 60));
+
+//   if (min < 10){ 
+//     min = "0" + min; 
+//   }
+//   if (sec < 10){ 
+//     sec  = "0" + sec;
+//   }
+
+//   return min + ':' + sec;
+// }
+
+
+
+// 	})
